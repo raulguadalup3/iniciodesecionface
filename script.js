3e3117108df5db303dd8a4429ff89bc9cace0a58
raulguadalup3 <165418920+raulguadalup3@users.noreply.github.com>
2024-04-03 15:42:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ FB.getLoginStatus(function(response) {
         userID:'{user-id}'
     }
 }
+
+FB.login(function(response){
+  // handle the response 
+});
+
+FB.login(function(response) {
+  // handle the response
+}, {scope: 'public_profile,email'});
+
+
+FB.login(function(response) {
+  if (response.status === 'connected') {
+    // Logged into your webpage and Facebook.
+  } else {
+    // The person is not logged into your webpage or we are unable to tell. 
+  }
+});
+
+FB.logout(function(response) {
+   // Person is now logged out
+});
